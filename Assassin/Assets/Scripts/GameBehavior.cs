85a48f3e03e3b7bfb5e1333f96b3142fe871b11c
Jeremy Rylicki <jeremy.rylicki@ndus.edu>
2020-07-10 13:51:34 -0500

    Unity Updated, Version 1.0 Created
    I couldn't download the older unity so y'all going to need to update.
    Version 1.0, super basic version of the game. Creates a list and mixes it up. then search by your name and target name will appear.

---
+++ Assassin/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameBehavior : MonoBehaviour
+{
+    private List<string> list = new List<string>();
+    public GameObject AddPlayerInputField;
+    public GameObject SearchPlayerInputField;
+    public TMP_Text PlayerAddedTxt;
+    public TMP_Text TargetTxt;
+
+    public void addPlayer()
+    {  
+        //takes inputted name and sets it to player
+       string player = AddPlayerInputField.GetComponent<TMP_InputField>().text;
+        //adds player to a list
+       list.Add(player);
+        //displays that player is added
+       PlayerAddedTxt.GetComponent<TMP_Text>().text = list[list.Count -1] + " was added to the game!";
+       AddPlayerInputField.GetComponent<TMP_InputField>().text = "";
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            string temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+
+    public void Search()
+    {
+        
+        //gets inputted name 
+        string searcher = SearchPlayerInputField.GetComponent<TMP_InputField>().text.ToLower();
+        int index;
+        //finds the searcher's index
+      
+        index = list.FindIndex(x => x.ToLower().Equals(searcher));
+        //if name doesn't exist 
+        if (index == -1)
+        {
+            TargetTxt.GetComponent<TMP_Text>().text = "Searched name doesn't exist, try again.";
+        }
+        else
+        {
+            //gives the player below them on the list as their target
+            int targetIndex = index + 1;
+            //checks if searcher is at the bottom of the list, if so make the target the player at the top of the list
+            if (targetIndex == list.Count)
+            {
+                targetIndex = 0;
+            }
+            //shows target and clears name from search bar
+            TargetTxt.GetComponent<TMP_Text>().text = "Target Player: " + list[targetIndex];
+            SearchPlayerInputField.GetComponent<TMP_InputField>().text = "";
+        }
+        
+    }
+
+
+}
