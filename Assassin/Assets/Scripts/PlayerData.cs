a874e4527a0811d1ac627a0049ddb69f68e6ffe9
Jdrylicki <jdrylicki@gmail.com>
2020-07-20 21:00:58 -0500

    Working to keep username after player ends game
    Username

---
+++ Assassin/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerData : MonoBehaviour
+{
+    public GameObject Username;
+    void Start()
+    {
+        Username.GetComponent<TMP_InputField>().text = PlayerPrefs.GetString("UserName");
+    }
+    public void save()
+    {
+        PlayerPrefs.DeleteKey("UserName");
+        PlayerPrefs.SetString("UserName", Username.GetComponent<TMP_InputField>().text);
+        
+    }
+      
+}
